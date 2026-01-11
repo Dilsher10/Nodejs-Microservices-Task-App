d552d1ec03d40b967690e607cef0c4fb0a0ad391
Dilsher <dilsher.dahri10@gmail.com>
2026-01-11 06:33:41 -0800

    changes

--- user-service/index.js
+++ user-service/index.js
@@ -35,7 +35,9 @@ app.post('/users', async (request, response) => {
   }
 })
 
-
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
   console.log(`User service listening on port ${port}`)
